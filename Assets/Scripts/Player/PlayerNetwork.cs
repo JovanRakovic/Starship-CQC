fb3b535539dd95d610e20c0f6c5565ffe82a55e9
Jovan Rakovic <jovanrakovic1456@gmail.com>
2024-06-30 10:15:24 +0200

    Initial commit

---
+++ Assets/Scripts/Player/PlayerNetwork.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerNetwork : NetworkBehaviour
+{
+    private Vector2 moveInput = Vector2.zero;
+    [SerializeField] float moveSpeed = 3f;
+
+    private void Start()
+    {
+        if(!IsOwner)
+        {
+            Destroy(GetComponent<PlayerInput>());
+            Destroy(this);
+        }
+    }
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.position += new Vector3(moveInput.x, 0, moveInput.y) * (moveSpeed * Time.deltaTime);
+    }
+
+    public void OnMove(InputValue value){ moveInput = value.Get<Vector2>(); }
+}
