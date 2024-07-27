35306ba09e32753cfa37bbee7999ac6a17c1a6a6
Jovan Rakovic <jovanrakovic1456@gmail.com>
2024-07-27 21:47:24 +0200

    Autopilot

--- Assets/Scripts/ships/Ship.cs
+++ Assets/Scripts/ships/Ship.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Unity.Netcode;
 
 public class Ship : MonoBehaviour
 {
@@ -16,14 +17,13 @@ public class Ship : MonoBehaviour
     //Boogaloo controls the secound set of axis (left/right, up/down)
     private Vector2 boogalooControls = Vector2.zero;
     private Vector2 cursorManuver = Vector2.zero;
-    private float throttle = 0f;
-
-    [SerializeField] private float mainThrusterStrength = 100f;
-    [SerializeField] private float boogalooThrustersStrength = 100f;
-    [SerializeField] private float turnStrength = 100f;
-    [SerializeField] private float rotationTrustersStrength = 100f;
+    [SerializeField] private float throttle = 0f;
+    [SerializeField] private float throttleDeadzone = 0.08f;
+    [SerializeField] private float thrusterStrength = 1000f;
+    [SerializeField] private float rollStrength = 1000f;
+    [SerializeField] private float rotationTrustersStrength = 1000f;
     [SerializeField] private float cursorSensitivity = 1f;
-    [SerializeField] private float cursorDeadzone = .05f;
+    [SerializeField] private float cursorDeadzone = .1f;
     [SerializeField] private float velocityLimit = 10f;
     [SerializeField] private float angularVelocityLimit = 10f;
 
@@ -49,20 +49,42 @@ public class Ship : MonoBehaviour
         HandleMovement();
     }
 
-
     private void HandleMovement()
     {
-        rigid.AddForce(transform.forward * (throttle * mainThrusterStrength * rigid.mass * Time.fixedDeltaTime), ForceMode.Force);
-        rigid.AddForce(transform.TransformDirection(boogalooControls) * (boogalooThrustersStrength * rigid.mass * Time.fixedDeltaTime), ForceMode.Force);
-        rigid.AddTorque(transform.TransformDirection(new Vector3(0,0,-mainControls.x)) * (turnStrength * Time.fixedDeltaTime * rigid.mass), ForceMode.Force);
-        if(cursorManuver.magnitude > cursorDeadzone && cameraFixed)
-            rigid.AddTorque(transform.TransformDirection(new Vector3(-cursorManuver.y, cursorManuver.x, 0) * (rotationTrustersStrength * rigid.mass * Time.fixedDeltaTime)), ForceMode.Force);
-        
+        //A lot of things are multiplied by these two, so thought I should save a bit of computing power by storing them in a variable
+        float standardMultiplication = rigid.mass * Time.fixedDeltaTime;
+
+        float deadzonedThrottle = 0;
+        if(Mathf.Abs(throttle) > throttleDeadzone) 
+            deadzonedThrottle = (throttle + ((throttle > 0)? -throttleDeadzone : throttleDeadzone)) / (1f - throttleDeadzone);
+        Vector3 targetDirection = transform.forward * deadzonedThrottle + transform.right * boogalooControls.x + transform.up * boogalooControls.y;
+        targetDirection = autopilot? Vector3.ClampMagnitude(targetDirection * velocityLimit - rigid.velocity, 1) : targetDirection.normalized;
+
+        Vector3 targetRoll = transform.forward * -mainControls.x;
+        Vector3 forwardProjection = Vector3.Project(rigid.angularVelocity, transform.forward);
+        targetRoll = autopilot? Vector3.ClampMagnitude(targetRoll * angularVelocityLimit - forwardProjection, 1) : targetRoll.normalized;
+
+        rigid.AddForce(targetDirection * (thrusterStrength * standardMultiplication), ForceMode.Force);
+        rigid.AddTorque(targetRoll * (rollStrength * standardMultiplication), ForceMode.Force);
+
+        Vector3 tourqeDirection = Vector3.zero;
+        if(cursorManuver.magnitude > cursorDeadzone)
+        {
+            tourqeDirection = transform.right * -cursorManuver.y + transform.up * cursorManuver.x;
+            tourqeDirection = (tourqeDirection - tourqeDirection.normalized * cursorDeadzone) / (1f - cursorDeadzone);
+        }
+
+        if(autopilot)
+            tourqeDirection = Vector3.ClampMagnitude(tourqeDirection * angularVelocityLimit - rigid.angularVelocity - forwardProjection, 1);
+            
+        rigid.AddTorque(tourqeDirection * (rotationTrustersStrength * standardMultiplication), ForceMode.Force);
+
         //Speed Limiting
         if(rigid.velocity.magnitude > velocityLimit)
-            rigid.AddForce(-rigid.velocity.normalized * (mainThrusterStrength * rigid.mass * Time.fixedDeltaTime), ForceMode.Force);
+            //I put a two in the equation as the player could "stall" the speed limit if they somehow got passed it in the first place
+            rigid.AddForce(-rigid.velocity.normalized * (thrusterStrength * 2 * standardMultiplication), ForceMode.Force);
         if(rigid.angularVelocity.magnitude > angularVelocityLimit)
-            rigid.AddTorque(-rigid.angularVelocity.normalized * (Time.fixedDeltaTime * turnStrength * Mathf.Rad2Deg * 100), ForceMode.Force);
+            rigid.AddTorque(-rigid.angularVelocity.normalized * (standardMultiplication * rollStrength), ForceMode.Force);
     }
 
     private void AddToThrottle(float add)
@@ -72,8 +94,8 @@ public class Ship : MonoBehaviour
     }
 
 #region INPUT
-    public void OnMove(InputValue value) {mainControls = value.Get<Vector2>();}
-    public void OnMove2ElectricBoogaloo(InputValue value) {boogalooControls = value.Get<Vector2>();}
+    public void OnMove(InputValue value) { mainControls = value.Get<Vector2>(); }
+    public void OnMove2ElectricBoogaloo(InputValue value) { boogalooControls = value.Get<Vector2>(); }
     public void OnDelta(InputValue value) 
     {
         cursorManuver += value.Get<Vector2>() * cursorSensitivity;
@@ -81,5 +103,6 @@ public class Ship : MonoBehaviour
             cursorManuver.Normalize();
         shipUI.SetManuverPointerPosition(cursorManuver);
     }
+    public void OnAutopilot() { autopilot = !autopilot; }
 #endregion
 }
